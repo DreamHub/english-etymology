27bbbf047974c86e51267baa27fc4012c1c3e4dc
chu.liangcai <790704125@qq.com>
2019-01-06 14:19:48 +0800

    对对对

--- 常见sql.sql
+++ 常见sql.sql
@@ -1,1 +1,14 @@
 -- 查询单词
+select * from etymology where word = 'in';
+
+--插入专有名词
+insert into etymology(word,meaning) values('paris','巴黎');
+
+--插入词缀
+insert into etymology(word,meaning,type,nonword) values('in','','affix','1');
+
+--插入普通单词
+insert into etymology(word,meaning,origin,type) values('intend','','','affix');
+
+--插入词源
+insert into etymology(word,meaning,type,nonword) values('in','','affix','1');
